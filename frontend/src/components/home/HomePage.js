c146a4edeb29fa3b5134c901bc26cdce14dd86ce
RF1984 <rafaeldcfernandes@gmail.com>
2019-11-20 16:54:47 +0100

    updated homepage layout

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import '../components.css';
 
 class HomePage extends Component {
     render() {
         return (
-            <div className="card">
-                <div className="card-body">
-                    <h4 className="card-title">FoodHub</h4>
+            <div className="card-hp">
+                <div className="card-bodyhp">
                     <p>Welcome too <strong>FoodHub</strong>! Here you will be able to:</p>
                     <ul>
                         <li>Share Food! <br/> (recipes, restaurants, experiences...)</li>
